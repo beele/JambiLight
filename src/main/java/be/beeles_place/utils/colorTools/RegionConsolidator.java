7ee0041eef1d7e539310727ec0135f982e008c04
Kevin Van den Abeele <kevin.vandenabeele@ida-mediafoundry.be>
2014-02-18 13:57:47 +0100

    Cleaned up RegionConsolidator
    - Cleaned up the RegionConsolidator class.
    - Added more documentation.

--- src/main/java/be/beeles_place/utils/colorTools/RegionConsolidator.java
+++ src/main/java/be/beeles_place/utils/colorTools/RegionConsolidator.java
@@ -56,70 +56,71 @@ public class RegionConsolidator {
 
         //Collect all regions per column into one pixel.
         for (int i = 0; i < width; i++) {
-            r = 0;
-            g = 0;
-            b = 0;
-            rr = 0;
-            gg = 0;
-            bb = 0;
+            //Reset color value variables.
+            r = g =b = rr = gg = bb = 0;
+            //Loop from column 0 + margin to half of the columns for the left side. (start column + margin <===> center column)
             for (int j = horizontalMargin; j < topDepth; j++) {
                 r += regions[i][j][0];
                 g += regions[i][j][1];
                 b += regions[i][j][2];
             }
+            //Calculate the average color values. (total added color values / number of loops)
             r /= (topDepth - horizontalMargin);
             g /= (topDepth - horizontalMargin);
             b /= (topDepth - horizontalMargin);
 
+            //Loop from the center column to the right end column - margin. (center column <===> end column - margin)
             for (int j = topDepth; j < (height - horizontalMargin); j++) {
                 rr += regions[i][j][0];
                 gg += regions[i][j][1];
                 bb += regions[i][j][2];
             }
+            //Calculate the average color values. (total added color values / number of loops)
             rr /= (bottomDepth - horizontalMargin);
             gg /= (bottomDepth - horizontalMargin);
             bb /= (bottomDepth - horizontalMargin);
 
-            //Top
+            //Top consolidated region.
             cRegions[i] = new int[]{r, g, b};
-            //Bottom
+            //Bottom consolidated region.
             cRegions[width + height - 2 + (width - i - 1)] = new int[]{rr, gg, bb};
         }
 
         //Collect all regions per row into one pixel.
         for (int m = 0; m < height; m++) {
-            r = 0;
-            g = 0;
-            b = 0;
-            rr = 0;
-            gg = 0;
-            bb = 0;
+            //Reset color value variables.
+            r = g =b = rr = gg = bb = 0;
+            //Loop from row 0 + margin to the half of the rows for the top side. (start row + margin <===> center row)
             for (int n = verticalMargin; n < leftDepth; n++) {
                 r += regions[n][m][0];
                 g += regions[n][m][1];
                 b += regions[n][m][2];
             }
+            //Calculate the average color values. (total added color values / number of loops)
             r /= (leftDepth - verticalMargin);
             g /= (leftDepth - verticalMargin);
             b /= (leftDepth - verticalMargin);
 
+            //Loop from the center row to the bottom end row - margin. (center row <===> end row - margin)
             for (int n = leftDepth; n < (width - verticalMargin); n++) {
                 rr += regions[n][m][0];
                 gg += regions[n][m][1];
                 bb += regions[n][m][2];
             }
+            //Calculate the average color values. (total added color values / number of loops)
             rr /= (rightDepth - verticalMargin);
             gg /= (rightDepth - verticalMargin);
             bb /= (rightDepth - verticalMargin);
 
-            //Left
+            //Left consolidated region.
             int tempIndex = width + (height - 2) + width + (height - 2) - m;
             tempIndex = tempIndex == finalRegionCount ? 0 : tempIndex;
             cRegions[tempIndex] = new int[]{r, g, b};
-            //Right
+            //Right consolidated region.
             cRegions[m + width - 1] = new int[]{rr, gg, bb};
         }
 
+        //Recalculate the corner regions. The loop logic above have some strange behaviour for the corners.
         int[] c1, c2;
         //TOP-LEFT CORNER
         c1 = cRegions[1];
@@ -141,6 +142,12 @@ public class RegionConsolidator {
         return cRegions;
     }
 
+    /**
+     * Calculates the average of two given regions/
+     * @param regionA Array of int containing the R/G/B colors for the first region.
+     * @param regionB Array of int containing the R/G/B colors for the second region.
+     * @return An array of int containing the averaged R/G/B values.
+     */
     private int[] averageRegions(int[] regionA, int[] regionB) {
         regionA[0] = (regionA[0] + regionB[0]) / 2;
         regionA[1] = (regionA[1] + regionB[1]) / 2;
