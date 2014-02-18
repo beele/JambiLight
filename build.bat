d2599cbb4f9adfef55d9c022bd3d704daf5d234f
beele <nabeele.80@gmail.com>
2014-02-18 19:03:55 +0100

    Fixed windows build script for folders with spaces

--- build.bat
+++ build.bat
@@ -3,6 +3,8 @@ echo ======================================
 echo Building with maven:
 echo ======================================
 echo ======================================
-call mvn install:install-file -Dfile=%cd%/lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
-call mvn install:install-file -Dfile=%cd%/lib/RXTXcomm.jar -DgroupId=com.google.code -DartifactId=RXTX -Dversion=2.2 -Dpackaging=jar
+set DIR=%cd%
+set DIR=^"%DIR%^"
+call mvn install:install-file -Dfile=%DIR%/lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
+call mvn install:install-file -Dfile=%DIR%/lib/RXTXcomm.jar -DgroupId=com.google.code -DartifactId=RXTX -Dversion=2.2 -Dpackaging=jar
 call mvn clean install
