f1f780d04896bb7e5b0b91764ce7fbd64774634e
beele <nabeele.80@gmail.com>
2014-03-16 16:56:15 +0100

    Edited build files and POM to reflect JSSC lib no more being local!

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ echo ======================================
 echo ======================================
 set DIR=%cd%
 set DIR=^"%DIR%^"
-call mvn install:install-file -Dfile=%DIR%/lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
+REM call mvn install:install-file -Dfile=%DIR%/lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
 call mvn install:install-file -Dfile=%DIR%/lib/RXTXcomm.jar -DgroupId=com.google.code -DartifactId=RXTX -Dversion=2.2 -Dpackaging=jar
 call mvn install:install-file -Dfile=%DIR%/lib/ScreenCapper.dll -DgroupId=be.beeles_place.code -DartifactId=JNIscreenCapMock -Dversion=1.0 -Dpackaging=dll
 call mvn clean install
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo  "======================================"
 echo  "Building with maven:"
 echo  "======================================"
 echo  "======================================"
-mvn install:install-file -Dfile=./lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
+#mvn install:install-file -Dfile=./lib/jssc.jar -DgroupId=com.google.code -DartifactId=jssc -Dversion=2.6 -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/RXTXcomm.jar -DgroupId=com.google.code -DartifactId=RXTX -Dversion=2.2 -Dpackaging=jar
 mvn install:install-file -Dfile=./lib/ScreenCapper.dll -DgroupId=be.beeles_place.code -DartifactId=JNIscreenCapMock -Dversion=1.0 -Dpackaging=dll
 mvn clean install
