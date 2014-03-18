51b93ca5687050d17fe00f836220c2dbec453381
beele <nabeele.80@gmail.com>
2014-03-18 19:39:55 +0100

    Updated XBMC python script
    It now captures the video data in 480p. Java implementation will follow
    after test about socket bandwidth have been concluded!

--- JambiLight_XBMC/plugin.service.jambilight/addon.py
+++ JambiLight_XBMC/plugin.service.jambilight/addon.py
@@ -1,9 +1,59 @@
 import sys
+import socket
+
+import xbmc
 import xbmcgui
 import xbmcplugin
- 
-title = "Hello World"
-text = "This is some text"
-time = 5000  # ms
- 
-xbmc.executebuiltin('Notification(%s, %s, %d)'%(title, text, time))
+
+
+def log(msg):
+    xbmc.log("### [%s] - %s" % ("JambiLight", msg,), level=xbmc.LOGDEBUG)
+
+
+#### Show startup notification.
+title = 'Startup'
+text = 'JambiLight plugin started!'
+time = 5000
+xbmc.executebuiltin('Notification(%s, %s, %d)' % (title, text, time))
+
+#### Start real code here!
+ALIVE = True
+HOST = 'localhost'
+PORT = 8080
+
+# Connect to the remote socket.
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((HOST, PORT))
+
+capture = xbmc.RenderCapture()
+capture.capture(720, 480, xbmc.CAPTURE_FLAG_CONTINUOUS)
+
+# Enter a loop for sending frames to the server application.
+while ALIVE:
+    xbmc.sleep(50)
+
+    try:
+        if xbmc.Player().isPlaying():
+            log("video playing!")
+
+            capture.waitForCaptureStateChangeEvent(100)
+            if capture.getCaptureState() == xbmc.CAPTURE_STATE_DONE:
+                #Get frame data, process it and send it!
+                sock.sendall(capture.getImage())
+                log("sending data")
+            else:
+                log("no image captured!")
+        else:
+            log("no video playing")
+
+        #data = sock.recv(1024)
+    except socket.error, e:
+        log("Socket error")
+        sock.close()
+
+# check to see if xbmc will shut down!
+if xbmc.abortRequested:
+    ALIVE = False
+    sock.close()
+    log("Socket closed")
+
