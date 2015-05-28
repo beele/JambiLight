4123f5c595a067340377755a7f45f46a901a5a7a
Kevin Van den Abeele <info@beeles-place.be>
2015-05-28 17:16:44 +0200

    CI why u no work!

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 echo  "======================================"
 echo  "======================================"
 echo  "Building with maven:"
@@ -9,4 +10,4 @@ mvn clean install -Dplatform.dependencies=true
 mv ./JambiLight_JAVA/target/dependency/JNIscreenCapMock-1.0.dll ./JambiLight_JAVA/target/ScreenCapper.dll
 echo "java -Xmx256m -jar JambiLight.jar" > ./JambiLight_JAVA/target/run.sh
 echo "java -Xmx128m -jar SocketDummy.jar" > ./JambiLight_XBMC/SocketDummy/target/run.sh
-read -p "Press return to continue..."
+echo "Build completed!"
